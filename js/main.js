0ab94212620c140342d8d098668d1af7910df30a
Bill Herman <wrherman@comcast.net>
2013-08-11 14:53:54 -0700

    add alert logic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
+
+
+
+
 var app = {
+
+showAlert: function (message, title) { 
+if (navigator.notification) { 
+navigator.notification.alert(message, null, title, 'OK'); 
+} else { 
+alert(title ? (title + ": " + message) : message); 
+} 
+},
 
     findByName: function() {
         console.log('findByName');
@@ -14,10 +26,18 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
+var self = this; 
+
+        this.store = new MemoryStore(function() { 
+self.showAlert('Store Initialized', 'Info'); 
+});
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };
 
 app.initialize();
+
+
+
+
